cb3c25e4493168ad84ffdb208e2cffda22579f2b
Divyam <divyamkumarp@gmail.com>
2024-08-09 02:38:27 +0530

    update: post api menu

--- src/app/api/menu/route.ts
+++ src/app/api/menu/route.ts
@@ -57,14 +57,16 @@ export async function POST(request: Request): Promise<Response> {
         "imgURL": "https://www.google.com",
         "timeSlot": "MORNING",
         "name": "Dosa"
+        "foodType": "VEG",
+        "category": "MAIN_COURSE"
         }
         */
 
         const body = await request.json();
 
         //safety checks
-        if (!body.day || !body.timeSlot || !body.name) {
-            return ERROR_RESPONSE("Invalid Request Body. Required fields: day, timeSlot, name", 400);
+        if (!body.day || !body.timeSlot || !body.name || !body.foodType || !body.category) {
+            return ERROR_RESPONSE("Invalid Request Body. Required fields: day, timeSlot, name, foodType, category", 400);
         }
 
 
@@ -105,12 +107,14 @@ export async function PATCH(request: Request): Promise<Response> {
         const body = await request.json();
 
         // Safety checks
-        if (!body.imgURL && !body.name) {
-            return ERROR_RESPONSE("Invalid Request Body. Required fields: name or imgURL", 400);
+        if (!body.imgURL && !body.name && !body.foodType && !body.category) {
+            return ERROR_RESPONSE("Invalid Request Body. Required fields: name or imgURL or foodType or category", 400);
         }
 
         const newName = body.name;
         const newImgURL = body.imgURL;
+        const newFoodType = body.foodType;
+        const newCategory = body.category;
 
         const findItem = await Menu.findById(id);
 
@@ -126,11 +130,13 @@ export async function PATCH(request: Request): Promise<Response> {
         });
 
         if (existingEntry) {
-            return ERROR_RESPONSE(`Menu item with name '${newName}' already exists for ${findItem.day} at ${findItem.timeSlot}.`, 400);
+            return ERROR_RESPONSE(`Menu item with name '${newName}' already exists for ${findItem.day} at ${findItem.timeSlot}.`, 409);
         }
 
         if(newName) findItem.name = newName;
         if(newImgURL) findItem.imgURL = newImgURL;
+        if(newFoodType) findItem.foodType = newFoodType;
+        if(newCategory) findItem.category = newCategory;
 
         await findItem.save();
 
